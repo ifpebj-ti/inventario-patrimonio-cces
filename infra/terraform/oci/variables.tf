d48104cd18ab6ab4d44661079f73e063ef369dd0
Pedro Lira <pedrovinilira2005@gmail.com>
2026-09-20 15:59:54 -0300

    infra: configure OCI Terraform provider

---
+++ infra/terraform/oci/variables.tf
@@ -0,0 +1,55 @@
+variable "tenancy_ocid" {
+  description = "OCID da tenancy OCI."
+  type        = string
+  sensitive   = true
+}
+
+variable "user_ocid" {
+  description = "OCID do usuario OCI usado pelo Terraform."
+  type        = string
+  sensitive   = true
+}
+
+variable "fingerprint" {
+  description = "Fingerprint da API key OCI."
+  type        = string
+  sensitive   = true
+}
+
+variable "private_key_path" {
+  description = "Caminho local da chave privada da API OCI."
+  type        = string
+  sensitive   = true
+}
+
+variable "region" {
+  description = "Regiao OCI onde os recursos serao provisionados."
+  type        = string
+}
+
+variable "compartment_ocid" {
+  description = "OCID do compartment inventarium."
+  type        = string
+  sensitive   = true
+}
+
+variable "ssh_public_key" {
+  description = "Chave publica SSH que sera adicionada nas VMs."
+  type        = string
+  sensitive   = true
+}
+
+variable "allowed_ssh_cidrs" {
+  description = "CIDRs autorizados a acessar SSH."
+  type        = list(string)
+}
+
+variable "allowed_web_cidrs" {
+  description = "CIDRs autorizados a acessar HTTP e HTTPS."
+  type        = list(string)
+}
+
+variable "allowed_admin_cidrs" {
+  description = "CIDRs autorizados a acessar ferramentas administrativas."
+  type        = list(string)
+}
