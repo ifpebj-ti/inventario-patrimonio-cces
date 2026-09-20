8d46c75b894b2e4bf18f580bcda98093577f332f
Pedro Lira <pedrovinilira2005@gmail.com>
2026-09-20 18:26:41 -0300

    infra: add OCI NAT gateway

--- infra/terraform/oci/network.tf
+++ infra/terraform/oci/network.tf
@@ -24,6 +24,24 @@ resource "oci_core_route_table" "public" {
   }
 }
 
+resource "oci_core_nat_gateway" "main" {
+  compartment_id = var.compartment_ocid
+  display_name   = "${var.project_name}-nat-gateway"
+  vcn_id         = oci_core_vcn.main.id
+}
+
+resource "oci_core_route_table" "private" {
+  compartment_id = var.compartment_ocid
+  display_name   = "${var.project_name}-private-route-table"
+  vcn_id         = oci_core_vcn.main.id
+
+  route_rules {
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = oci_core_nat_gateway.main.id
+  }
+}
+
 resource "oci_core_security_list" "subnet_baseline" {
   compartment_id = var.compartment_ocid
   display_name   = "${var.project_name}-subnet-baseline-security-list"
@@ -52,6 +70,7 @@ resource "oci_core_subnet" "observability_private" {
   display_name               = "${var.project_name}-observability-private-subnet"
   dns_label                  = "obsprivate"
   prohibit_public_ip_on_vnic = true
+  route_table_id             = oci_core_route_table.private.id
   security_list_ids          = [oci_core_security_list.subnet_baseline.id]
   vcn_id                     = oci_core_vcn.main.id
 }
